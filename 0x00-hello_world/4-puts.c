7ab89c40d8d449180a04efe47917bfe29f3eb287
Victor <victorayodeji43@gmail.com>
2022-09-09 07:52:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#include <stdio.h>
+/** 
+ * Print the string in the main function
+ *
+ * Description: Using the main function
+ * This program prints programming is like bulding a 
+ * multilingual puzzle
+ */ 
+int main(void)
+{   puts("Programming is like building a multilingual puzzle/n")
+    return(0); 
+} 
+
