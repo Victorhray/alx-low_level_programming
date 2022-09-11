fcf7190796616bce0597dd108f9f917864b79d78
Victor <victorayodeji43@gmail.com>
2022-09-11 12:03:03 -0700

    print size of memory types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+/**
+ * main - print the string in the put function
+ * Description: prints the size of various types on the computer
+ * it is compiled and run on
+ * Return: 0
+ */
+int main(void)
+{	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", size of (c));
+	printf("Size of an int: %ld byte(s)\n", size of (i));
+	printf("Size of a long int: %ld byte(s)"\n size of (li));
+	printf("Size of a long long int: %ld byte(s)"\n size of (lli));
+	printf("Size of a float: %ld byte(s)"\n size of (f));
+	return (0);
+}
