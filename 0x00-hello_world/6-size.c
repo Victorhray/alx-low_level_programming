68a04b36c16c21905cb9ca0327154420707dbd5c
Victor <victorayodeji43@gmail.com>
2022-09-11 12:12:31 -0700

    Print various size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - print the string in the put function
- * Description: prints the size of various types on the computer
- * it is compiled and run on
+ *
+ * Description: prints the size of various types on the
+ * computer it is compiled and run on
  * Return: 0
  */
 int main(void)
@@ -13,10 +14,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", size of (c));
-	printf("Size of an int: %ld byte(s)\n", size of (i));
-	printf("Size of a long int: %ld byte(s)"\n size of (li));
-	printf("Size of a long long int: %ld byte(s)"\n size of (lli));
-	printf("Size of a float: %ld byte(s)"\n size of (f));
+	printf("Size of a char: %ld byte(s)\n", size of(c));
+	printf("Size of an int: %ld byte(s)\n", size of(i));
+	printf("Size of a long int: %ld byte(s)"\n size of(li));
+	printf("Size of a long long int: %ld byte(s)"\n size of(lli));
+	printf("Size of a float: %ld byte(s)"\n size of(f));
 	return (0);
 }
