3bbc9a72b6e8a3ba6cbc3207c8b30c5028485093
Victor <victorayodeji43@gmail.com>
2022-09-11 13:23:43 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print a text
+ *
+ * Description: using the main function
+ * this programm will print
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a new line
+ * Return: 1
+ */
+int main(void)
+{	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
