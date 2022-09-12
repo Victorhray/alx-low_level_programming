b20bf1a313984b105e1c588240857eacda0536d9
Victor <victorayodeji43@gmail.com>
2022-09-12 14:12:09 -0700

    List alphabelt in lowercas

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this program  prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while
+		(c <= 'z')
+		putchar(c);
+		c++;
+		putchar(\n");
+	return (0);
+}
